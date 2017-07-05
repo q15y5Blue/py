5d776007bb333da25438cab1407e4ceaaed87124
q15y5Blue <q15y5@outlook.com>
2017-07-05 10:53:28 +0800

    new

--- moocDemo/demo/execute/exepy.py
+++ moocDemo/demo/execute/exepy.py
@@ -3,9 +3,12 @@
 from demo.execute.getUrls import getUrls
 from demo.execute.getNews import getNewsByUrl
 from demo.dao import NewsDao
+from demo.until import pyMySqlDB
 url="http://news.163.com/rank/"
 list=getUrls(url)
 print("url队列长度",list.__len__())
 for l in list :
     newsObj = getNewsByUrl(l)
     NewsDao.updateNewsObj(newsObj)
+connect=pyMySqlDB.connectionClazz.connection
+connect.close()
--- moocDemo/demo/execute/getNews.py
+++ moocDemo/demo/execute/getNews.py
@@ -4,14 +4,9 @@ from bs4 import BeautifulSoup
 import re
 from demo.entity.news import News
 from datetime import datetime
-from demo.execute.getUrls import getUrls
 from demo.dao import NewsDao
 #在beautifulsoup中prettify()才是获取字符串的方法
 #根据Url获取News对象
-#from demo.execute.getUrls import getUrls
-#url=url= "http://news.163.com/rank/"
-#list = getUrls(url)
-#http://news.163.com/17/0629/01/CO2I1NJR00018AOP.html
 #根据url获取详细的news对象
 url ="http://news.163.com/17/0629/01/CO2I1NJR00018AOP.html";
 
@@ -42,10 +37,6 @@ def getNewsByUrl(url):
     ti=re.search("\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d",author.__str__())
     datetimeObj=datetime.strptime(ti.group(0),"%Y-%m-%d %H:%M:%S")
     news.tim=datetimeObj
-    #datetimeStamp=datetime.timestamp(datetimeObj)
-    #news.tim=datetimeStamp#时间get datetimeStamp 是float类型
-    print(datetimeObj.__str__())#2017-06-29 01:58:01
-    print(news.tim)#1498672681.0
     
     #author get
     news._authors=re.search("(?<=>).*?(?=</a>)",author.__str__()).group(0)
@@ -54,9 +45,3 @@ def getNewsByUrl(url):
     print("爬到了一个新闻  标题为:",news.title)
     return news
 
-def testANews():
-    newt=getNewsByUrl(url)
-    NewsDao.updateNewsObj(newt)
-
-def testSelectAllNews():
-    return NewsDao.selectAllNews()
