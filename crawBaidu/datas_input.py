c06da7aa0c27fb835741e5a56d00a582480942c8
q15y5Blue <q15y5@outlook.com>
2018-05-11 14:50:21 +0800

    Mysql 数据库链接

---
+++ crawBaidu/datas_input.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+import MySQLdb
+import json
+
+def get_connect():
+    db_connect = MySQLdb.connect("localhost", "root", "qiuyu", "qiuyu", charset="utf8")
+    return db_connect
+
+def update_info(connect,sql):
+    cursor = connect.cursor()
+    cursor.execute(sql)
+    try:
+        connect.commit()
+    except:
+        connect.rollback()
