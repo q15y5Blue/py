b4bbdd97da10f9a607445307a1365cce7c29e06c
q15y5Blue <q15y5@outlook.com>
2018-10-11 16:55:24 +0800

    ML arithmetic of knn

---
+++ ml/knn/knn.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# 精度高 对异常值不敏感 无数据输入假定 缺点计算复杂度、空间复杂度高 数值型,标称型
+from numpy import *
+import operator
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify(inX, dataSet, lables, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet   # tile 将数组inX 重复N次
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1) # 矩阵行向量相加  axis＝0表示按列相加
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()# 将矩阵按照axis排序，并返回排序后的下标
+    classCount = {}
+    for i in range(k):
+        voteIlable = lables[sortedDistIndicies[i]]
+        classCount[voteIlable] = classCount.get(voteIlable, 0)+1
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOfLines = fr.readlines()
+    numberOfLines = len(arrayOfLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOfLines:
+        line = line.strip()
+        listFromLine = line.strip("\t")
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index +=1
+    return returnMat,classLabelVector
+
+
+if __name__ == "__main__":
+    group, lables= createDataSet()
+    result = classify([0.0, 0.0],group, lables, 3)
+    print(result)
