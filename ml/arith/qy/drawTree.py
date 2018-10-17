cb99088a28f505890e594263a734828a2ea54987
q15y5Blue <q15y5@outlook.com>
2018-10-17 16:05:23 +0800

    Tree 绘制 尚有BUG

--- ml/arith/qy/drawTree.py
+++ ml/arith/qy/drawTree.py
@@ -18,7 +18,49 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
                             textcoords='axes fraction', va="center", ha="center", bbox=nodeType,
                             arrowprops=arrow_args)
 
+# 创建树 同createTree
+def retrieveTree(i):
+    listOfTree=[{'no suerfacing':{0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                {'no suerfacing':{0:'no', 1:{'flippers':{
+                    0:{'head':{0:'no', 1:'yes'}}, 1:'no'}}}}]
+    return listOfTree[i]
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0]-cntrPt[0])/2.0 +cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2.0 +cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numberLeafs = getNumerLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff +(1.0+float(numberLeafs))/2.0/plotTree.totalW,plotTree.yOff)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1,facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, framon=False, **axprops)
+    plotTree.totalW = float(getNumerLeafs(inTree))
+    plotTree.totalD = float(getNumerLeafs(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff =1.0
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
+
 if __name__ == "__main__":
     dataSet, labels = createDataSet()
-    print(createTree(dataSet, labels))
-    print(createPlot())
+    print(retrieveTree(1))
+    createPlot(retrieveTree(1))
