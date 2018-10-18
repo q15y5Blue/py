8df9dcc34c0877ef4a2f53f2e6d72472e4bc81ab
q15y5Blue <q15y5@outlook.com>
2018-10-18 15:44:24 +0800

    修复了决策树绘制的BUG

--- ml/arith/qy/drawTree.py
+++ ml/arith/qy/drawTree.py
@@ -5,16 +5,17 @@ from ml.arith.qy.tree import *
 decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
-def createPlot():
+def createPlotTest():
     fig = plt.figure(1, facecolor="white")
     fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('a decsion node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+    ax1 = plt.subplot(111, frameon=False)
+    ax1.annotate("sdasdasd", xy=(0.1,0.5), xycoords='axes fraction', xytext=(0.5,0.5),
+                            textcoords='axes fraction', va="center", ha="center", bbox=decisionNode,
+                            arrowprops=arrow_args)
     plt.show()
 
-def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt,
+def plotNode(nodeTxt, starNode, textNode, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=starNode, xycoords='axes fraction', xytext=textNode,
                             textcoords='axes fraction', va="center", ha="center", bbox=nodeType,
                             arrowprops=arrow_args)
 
@@ -25,42 +26,43 @@ def retrieveTree(i):
                     0:{'head':{0:'no', 1:'yes'}}, 1:'no'}}}}]
     return listOfTree[i]
 
+# 绘制线上的标记 比如 0,1
 def plotMidText(cntrPt, parentPt, txtString):
-    xMid = (parentPt[0]-cntrPt[0])/2.0 +cntrPt[0]
+    xMid= (parentPt[0]-cntrPt[0])/2.0 +cntrPt[0]
     yMid = (parentPt[1]-cntrPt[1])/2.0 +cntrPt[1]
     createPlot.ax1.text(xMid, yMid, txtString)
 
+# 绘制树形图
 def plotTree(myTree, parentPt, nodeTxt):
     numberLeafs = getNumerLeafs(myTree)
-    depth = getTreeDepth(myTree)
     firstStr = list(myTree.keys())[0]
-    cntrPt = (plotTree.xOff +(1.0+float(numberLeafs))/2.0/plotTree.totalW,plotTree.yOff)
-    plotMidText(cntrPt,parentPt,nodeTxt)
-    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    cntrPt = (plotTree.xOff + (1.0+float(numberLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, parentPt, cntrPt, decisionNode)
     secondDict = myTree[firstStr]
     plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':
+        if type(secondDict[key]).__name__ == 'dict':
             plotTree(secondDict[key], cntrPt, str(key))
         else:
             plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
-            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotNode(secondDict[key], cntrPt, (plotTree.xOff, plotTree.yOff), leafNode)
             plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
     plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
 
 def createPlot(inTree):
-    fig = plt.figure(1,facecolor='white')
+    fig = plt.figure(1, facecolor='white')
     fig.clf()
     axprops = dict(xticks=[], yticks=[])
-    createPlot.ax1 = plt.subplot(111, framon=False, **axprops)
-    plotTree.totalW = float(getNumerLeafs(inTree))
-    plotTree.totalD = float(getNumerLeafs(inTree))
-    plotTree.xOff = -0.5/plotTree.totalW
-    plotTree.yOff =1.0
-    plotTree(inTree,(0.5,1.0),'')
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)#  **axprops
+    plotTree.totalW = float(getNumerLeafs(inTree))   # 树的宽度    用于计算放置判断结点的位置
+    plotTree.totalD = float(getTreeDepth(inTree))    # 树的深度
+    plotTree.xOff = -0.5/plotTree.totalW       # 追踪已经绘制的结点位置
+    plotTree.yOff = 1.0                        # 放置下一个结点的位置
+    plotTree(inTree, (0.5, 1.0), '')
     plt.show()
 
 if __name__ == "__main__":
     dataSet, labels = createDataSet()
-    print(retrieveTree(1))
-    createPlot(retrieveTree(1))
+    myTree = retrieveTree(1)
+    createPlot(myTree)
