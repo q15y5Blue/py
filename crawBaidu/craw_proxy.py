05b2326b41d24131fa48377ea02af10127d46468
q15y5Blue <q15y5@outlook.com>
2018-10-24 16:58:34 +0800

    爬代理 修改了之前冗余的代码,

--- crawBaidu/craw_proxy.py
+++ crawBaidu/craw_proxy.py
@@ -1,40 +1,54 @@
 # coding:utf-8
 from crawBaidu.headers import headers
-
+import crawBaidu.db as din
 from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "http://www.66ip.cn/1.html"
 
-class obj_ip():
+class netProtocol():
     def __init__(self):
-        self.ip_location = ""
+        self.location = ""
         self.port = ""
+    def __str__(self):
+        return self.location + ":"+ self.port
 
-
-def craw_ips():
+# 获取每一个页的ipList
+def craw_ipList(url):
     req = requests.get(url, headers=headers)
     req.encoding = "gb2312"
     soup = BeautifulSoup(req.text, "html.parser")
     table_li = soup.find_all("table", width="100%").__getitem__(0)
-    exp_ip = re.compile("(\d{1,3}.){3}\d{1,3}")
-    exp_port = re.compile("(^\s)*(\d+)\n")
+    exp_ip = re.compile("(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)")
+    exp_port = re.compile("(?<=<td>)\d{1,5}(?=</td>)")
     tr_li = table_li.find_all("tr")
+    ipList = []
     for tr in tr_li:
-        print(tr.prettify())
-        temp_str = tr.prettify()
-        location_ip = exp_ip.search(temp_str).__getitem__(0)
-        location_port = exp_port.findall(temp_str).__getitem__(1)
-        print(location_port)
-        if location_ip is not None and location_port is not None:
-            obj_ip.ip_location =location_ip
-            obj_ip.port =location_port
-            # print(exp_ip.search(tr.prettify()))
+        strs = str(tr)
+        location_ip = exp_ip.search(strs)
+        location_port = exp_port.search(strs)
+        if type(location_ip).__name__ == "SRE_Match" and type(location_port).__name__=="SRE_Match":
+            ip = netProtocol()
+            ip.location=str(location_ip.group(0))
+            ip.port=str(location_port.group(0))
+            ipList.append(ip)
+    return ipList
 
-            #@print(tr.prettify())
-            # print(exp_ip.search(tr.string))
+def import_date(list):
+    connect = din.get_connect()
+    for lis in list:
+        sql = "insert into proxy(location,port) values ('%s','%s')" % (lis.location,lis.port)
+        din.update_info(connect, sql)
+    connect.close()
+
+def pagingCraw():
+    list = []
+    for x in range(1, 11):
+        url = "http://www.66ip.cn/%s.html" % (x)
+        list.extend(craw_ipList(url))
+    import_date(list)
+    # return list
 
 
 if __name__ == '__main__':
-    craw_ips()
+    pagingCraw()
